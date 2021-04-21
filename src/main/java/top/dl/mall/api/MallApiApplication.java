893609a1a345cf13c8aa93b52794a9d33df4e955
MangGuowjh <1678395645@qq.com>
2021-04-21 18:21:38 +0800

    楼楼商城api初始化

---
+++ src/main/java/top/dl/mall/api/MallApiApplication.java
@@ -0,0 +1,16 @@
+package top.dl.mall.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Denglin
+ */
+@SpringBootApplication
+public class MallApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MallApiApplication.class, args);
+    }
+
+}
