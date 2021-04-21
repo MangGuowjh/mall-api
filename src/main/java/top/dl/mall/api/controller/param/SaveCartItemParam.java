35a483b7369a4224ab730906d61e475df0221d56
MangGuowjh <1678395645@qq.com>
2021-04-21 22:20:07 +0800

    左右的param参数类

---
+++ src/main/java/top/dl/mall/api/controller/param/SaveCartItemParam.java
@@ -0,0 +1,16 @@
+package top.dl.mall.api.controller.param;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+import java.io.Serializable;
+
+/**
+ * 添加购物项param
+ * @author Denglin
+ */
+@Data
+public class SaveCartItemParam implements Serializable {
+    @ApiModelProperty("商品数量")
+    private Integer goodsCount;
+    @ApiModelProperty("商品id")
+    private Long goodsId;
+}
---
+++ src/main/java/top/dl/mall/api/controller/param/SaveOrderParam.java
@@ -0,0 +1,16 @@
+package top.dl.mall.api.controller.param;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+import java.io.Serializable;
+
+/**
+ * 保存订单param
+ * @author Denglin
+ */
+@Data
+public class SaveOrderParam implements Serializable {
+    @ApiModelProperty("订单项id数组")
+    private Long[] cartItemIds;
+    @ApiModelProperty("地址id")
+    private Long addressId;
+}
