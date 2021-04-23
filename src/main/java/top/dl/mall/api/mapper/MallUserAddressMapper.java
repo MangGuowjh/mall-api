15e09199afc8ab7004031bd3a676292270ca5b9a
MangGuowjh <1678395645@qq.com>
2021-04-23 09:58:58 +0800

    个人地址接口用户接口

--- src/main/java/top/dl/mall/api/mapper/MallUserAddressMapper.java
+++ src/main/java/top/dl/mall/api/mapper/MallUserAddressMapper.java
@@ -1,24 +1,31 @@
 package top.dl.mall.api.mapper;
 
 import top.dl.mall.api.entity.MallUserAddress;
-
+import java.util.List;
 /**
+ * @author Denglin
  * @Entity top.dl.mall.api.entity.MallUserAddress
  */
 public interface MallUserAddressMapper {
 
     int deleteByPrimaryKey(Long id);
-
     int insert(MallUserAddress record);
-
     int insertSelective(MallUserAddress record);
-
     MallUserAddress selectByPrimaryKey(Long id);
-
     int updateByPrimaryKeySelective(MallUserAddress record);
-
     int updateByPrimaryKey(MallUserAddress record);
-
+    /**
+     * 根据用户id获取默认收货地址
+     * @param userId 用户id
+     * @return 默认收货地址
+     */
+    MallUserAddress getMyDefaultAddress(Long userId);
+    /**
+     * 根据用户id获取用户的收货地址列表
+     * @param userId 用户id
+     * @return 收货地址列表
+     */
+    List<MallUserAddress> findMyAddressList(Long userId);
 }
 
 
---
+++ src/test/java/top/dl/mall/api/service/MallUserAddressServiceTest.java
@@ -0,0 +1,32 @@
+package top.dl.mall.api.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MallUserAddressServiceTest {
+
+    @Test
+    void getMyAddresses() {
+    }
+
+    @Test
+    void saveUserAddress() {
+    }
+
+    @Test
+    void updateMallUserAddress() {
+    }
+
+    @Test
+    void getMallUserAddressById() {
+    }
+
+    @Test
+    void getMyDefaultAddressByUserId() {
+    }
+
+    @Test
+    void deleteById() {
+    }
+}
