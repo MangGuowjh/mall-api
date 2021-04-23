877bf1bc3f429c6160a346c399ce588ed1f10a3f
MangGuowjh <1678395645@qq.com>
2021-04-23 08:40:29 +0800

    MallUserService实现及测试

---
+++ src/main/java/top/dl/mall/api/service/MallUserService.java
@@ -0,0 +1,37 @@
+package top.dl.mall.api.service;
+
+import top.dl.mall.api.controller.param.MallUserUpdateParam;
+
+/**
+ * @description: 用户Service接口
+ * @author Denglin
+ */
+public interface MallUserService {
+    /**
+     * 用户注册
+     * @param loginName 登录名
+     * @param password  密码
+     * @return String
+     */
+    String register(String loginName, String password);
+    /**
+     * 登录
+     * @param loginName   登录名
+     * @param passwordMd5 md5加密密码
+     * @return String
+     */
+    String login(String loginName, String passwordMd5);
+    /**
+     * 用户信息修改
+     * @param mallUser 修改用户的参数实体
+     * @param userId   用户id
+     * @return boolean
+     */
+    Boolean updateUserInfo(MallUserUpdateParam mallUser, Long userId);
+    /**
+     * 退出登录
+     * @param userId 用户id
+     * @return boolean
+     */
+    Boolean logout(Long userId);
+}
